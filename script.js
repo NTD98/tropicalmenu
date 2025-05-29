9a6381fe5e91bec73002860a9d65fd921bab89ff
datnt <16520199@gm.uit.edu.vn>
2025-05-29 17:24:41 +0700

    add cacao add carousel

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function handleImageError(img,imgUrl) {
-    if (imgUrl !== img.getAttribute('data-placeholder')) {
-        img.src = imgUrl;
-    }
+const carouselContainer = document.querySelector('.carousel-container');
+const slides = document.querySelectorAll('.carousel-slide');
+const prevBtn = document.querySelector('.carousel-btn.prev');
+const nextBtn = document.querySelector('.carousel-btn.next');
+
+let currentIndex = 0;
+
+function updateCarousel() {
+    const slideWidth = slides[0].clientWidth;
+    carouselContainer.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
 }
+
+nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    updateCarousel();
+});
+
+window.addEventListener('resize', updateCarousel);
